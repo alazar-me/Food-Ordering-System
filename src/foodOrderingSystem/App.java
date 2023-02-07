79c5182ef5f7d059ea386085ad16f41c11c31b34
alazar-me <almaxvsbeko@gmail.com>
2023-02-07 02:04:06 +0000

    Main Menu done

---
+++ src/foodOrderingSystem/App.java
@@ -0,0 +1,8 @@
+package foodOrderingSystem;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Welcome welcome = new Welcome();
+        welcome.showWelcomeGUI();
+    }
+}
